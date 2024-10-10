f528d166b7998c215e4cd2964fe92924b97b1fd5
nahuelz <nahuel.zanelli@hotmail.com>
2024-10-10 16:22:53 -0300

    Vista fullscreen

--- public/js/app/siembra/index.js
+++ public/js/app/siembra/index.js
@@ -19,8 +19,8 @@ var KTCalendarListView = function() {
                     right: 'dayGridDay'
                 },
                 hiddenDays:  [ 0 ],
-                height: 1200,
-                contentHeight: 1200,
+                height: 800,
+                contentHeight: 750,
                 aspectRatio: 3,  // see: https://fullcalendar.io/docs/aspectRatio
 
                 views: {
@@ -30,7 +30,7 @@ var KTCalendarListView = function() {
                 defaultView: 'dayGridDay',
                 defaultDate: TODAY,
 
-                editable: false,
+                editable: true,
                 eventLimit: true, // allow "more" link when too many events
                 navLinks: true,
                 eventSources: [
@@ -146,6 +146,17 @@ var KTCalendarListView = function() {
                     element.css('min-height', '75px');
                     element.find('.fc-title').css('font-size', '1.1rem');
                     element.find('.fc-content').css('margin-top', '1%');
+                    if (info.event.extendedProps && info.event.extendedProps.description) {
+                        if (element.hasClass('fc-day-grid-event')) {
+                            element.data('content', info.event.extendedProps.description);
+                            element.data('placement', 'top');
+                            KTApp.initPopover(element);
+                        } else if (element.hasClass('fc-time-grid-event')) {
+                            element.find('.fc-title').append('<div class="fc-description">' + info.event.extendedProps.description + '</div>');
+                        } else if (element.find('.fc-list-item-title').lenght !== 0) {
+                            element.find('.fc-list-item-title').append('<div class="fc-description">' + info.event.extendedProps.description + '</div>');
+                        }
+                    }
                 },
                 eventDrop: function(info) {
                     dialogFinalizarForm = '\
