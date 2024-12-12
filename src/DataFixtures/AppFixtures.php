9e9fa8e8d438a94249985fb2766a1443e6a35997
nahuelz <nahuel.zanelli@hotmail.com>
2024-12-12 14:30:56 -0300

    .

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -215,6 +215,37 @@ class AppFixtures extends Fixture
         $statement->execute();
 
         $connection = $manager->getConnection();
+        $statement = $connection->prepare("create definer = root@localhost view view_planificacion as
+                                            select `pp`.`id`                                                                                                 AS `id`,
+                                                   concat(`tp`.`nombre`, ' ', `tsp`.`nombre`, ' ', `tv`.`nombre`)                                            AS `nombreCompleto`,
+                                                   concat(`pp`.`id`, ' - ', `tv`.`nombre`, ' (', `pp`.`cantidad_bandejas_pedidas`, ' x', `tb`.`nombre`,
+                                                          ')')                                                                                               AS `nombreCorto`,
+                                                   concat(`pp`.`cantidad_bandejas_pedidas`, ' (', `tb`.`nombre`, ')')                                        AS `cantidadTipoBandejabandeja`,
+                                                   `pp`.`cantidad_bandejas_pedidas`                                                                          AS `cantidadBandejas`,
+                                                   `tb`.`nombre`                                                                                             AS `tipoBandeja`,
+                                                   concat(`u`.`nombre`, ' ', `u`.`apellido`)                                                                 AS `cliente`,
+                                                   `epp`.`nombre`                                                                                            AS `estado`,
+                                                   `epp`.`color`                                                                                             AS `colorEstado`,
+                                                   concat(`epp`.`class_name`, ' ', `tp`.`nombre`, ' ', `epp`.`nombre`)                                       AS `className`,
+                                                   `pp`.`fecha_siembra_planificacion`                                                                        AS `fechaSiembraPlanificacion`,
+                                                   `tp`.`nombre`                                                                                             AS `tipoProducto`,
+                                                   concat(`tp`.`nombre`, ' ', `tsp`.`nombre`, ' ', `tv`.`nombre`, ' ', `pp`.`cantidad_bandejas_pedidas`, ' (',
+                                                          `tb`.`nombre`, ')', ', Cliente: ', `u`.`nombre`, ' ',
+                                                          `u`.`apellido`)                                                                                    AS `descripcion`
+                                            from ((((((((`babyplant2`.`pedido` `p` left join `babyplant2`.`pedido_producto` `pp`
+                                                         on (`p`.`id` = `pp`.`id_pedido`)) left join `babyplant2`.`tipo_variedad` `tv`
+                                                        on (`pp`.`id_tipo_variedad` = `tv`.`id`)) left join `babyplant2`.`tipo_sub_producto` `tsp`
+                                                       on (`tv`.`id_tipo_sub_producto` = `tsp`.`id`)) left join `babyplant2`.`tipo_producto` `tp`
+                                                      on (`tsp`.`id_tipo_producto` = `tp`.`id`)) left join `babyplant2`.`tipo_bandeja` `tb`
+                                                     on (`pp`.`id_tipo_bandeja` = `tb`.`id`)) left join `babyplant2`.`usuario` `u`
+                                                    on (`p`.`id_cliente` = `u`.`id`)) left join `babyplant2`.`estado_pedido` `ep`
+                                                   on (`ep`.`id` = `p`.`id_estado_pedido`)) left join `babyplant2`.`estado_pedido_producto` `epp`
+                                                  on (`epp`.`id` = `pp`.`id_estado_pedido_producto`))
+                                            where `pp`.`fecha_baja` is null
+                                              and `pp`.`id_estado_pedido_producto` in (1, 2);");
+        $statement->execute();
+
+        $connection = $manager->getConnection();
         $statement = $connection->prepare("create definer = root@localhost view view_salida_camara as
                                             select `pp`.`id`                                                                    AS `id`,
                                                    `tp`.`id`                                                                    AS `idProducto`,
@@ -317,11 +348,6 @@ class AppFixtures extends Fixture
                                             from `babyplant2`.`tipo_usuario` `tu`;");
         $statement->execute();
 
-        $connection = $manager->getConnection();
-        $statement = $connection->prepare("create definer = root@localhost view view_tipo_usuario as
-                                            select `tu`.`id` AS `id`, `tu`.`nombre` AS `nombre`, `tu`.`habilitado` AS `habilitado`
-                                            from `babyplant2`.`tipo_usuario` `tu`;");
-        $statement->execute();
 
         $connection = $manager->getConnection();
         $statement = $connection->prepare("create definer = root@localhost view view_tipo_variedad as
