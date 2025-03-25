f36673ee290cb9a35ceeccc7b77b51d4bffcaf19
nahuelz <nahuel.zanelli@hotmail.com>
2025-03-25 20:17:05 -0300

    Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -611,10 +611,10 @@ BEGIN
         er.color                                                       AS colorEstado,
         er.id                                                          AS idEstado
     FROM remito r
-        LEFT JOIN entrega e on e.id = r.id_entrega
+        LEFT JOIN entrega e on e.id_remito = r.id
         LEFT JOIN entrega_producto rp ON e.id = rp.id_entrega
         LEFT JOIN pedido_producto pp ON pp.id = rp.id_pedido_producto
-        LEFT JOIN usuario u ON e.id_cliente = u.id
+        LEFT JOIN usuario u ON r.id_cliente = u.id
         LEFT JOIN tipo_variedad tv ON tv.id = pp.id_tipo_variedad
         LEFT JOIN tipo_sub_producto tsp ON tsp.id = tv.id_tipo_sub_producto
         LEFT JOIN tipo_producto tp ON tp.id = tsp.id_tipo_producto
@@ -629,38 +629,89 @@ END;");
         $statement->execute();
 
         $statement = $connection->prepare("create
-    definer = root@localhost procedure sp_index_pedido(IN _fechaDesde datetime, IN _fechaHasta datetime, IN _idCliente int)
+    definer = root@localhost procedure sp_index_reserva(IN _fechaDesde datetime, IN _fechaHasta datetime, IN _idCliente int)
 BEGIN
     SELECT DISTINCT
-        `r`.`id`                                                       AS `idRemito`,
+        `r`.`id`                                                       AS `idReserva`,
+        `r`.`id_entrega`                                               AS `idEntrega`,
         `pp`.`id`                                                      AS `idPedidoProducto`,
         CONCAT(pp.numero_orden,' ',substr(`tp`.`nombre`, 1, 3))        AS ordenSiembra,
         `r`.`fecha_creacion`                                           AS `fechaCreacion`,
         concat(`u`.`nombre`, ', ', `u`.`apellido`)                     AS `cliente`,
         u.id                                                           AS `idCliente`,
+        concat(`ur`.`nombre`, ', ', `ur`.`apellido`)                   AS `clienteReserva`,
+        ur.id                                                          AS `idClienteReserva`,
         concat(`tp`.`nombre`, ' ', `tsp`.`nombre`, ' ', `tv`.`nombre`) AS `nombreProductoCompleto`,
         `tp`.`nombre`                                                  AS `nombreProducto`,
-        `rp`.`cantidad_bandejas`                                       AS `cantidadBandejas`,
-        `rp`.`precio_unitario`                                         AS `precioUnitario`,
-        `rp`.`precio_unitario`*`rp`.`cantidad_bandejas`                AS `precioSubTotal`,
-        precio_total.total                                             AS `precioTotal`,
+        `r`.`cantidad_bandejas`                                        AS `cantidadBandejas`,
         er.nombre                                                      AS estado,
         er.color                                                       AS colorEstado,
-        er.id                                                          AS idEstado
-    FROM remito r
-        LEFT JOIN entrega e on e.id = r.id_entrega
-        LEFT JOIN entrega_producto rp ON e.id = rp.id_entrega
-        LEFT JOIN pedido_producto pp ON pp.id = rp.id_pedido_producto
-        LEFT JOIN usuario u ON r.id_cliente = u.id
-        LEFT JOIN tipo_variedad tv ON tv.id = pp.id_tipo_variedad
-        LEFT JOIN tipo_sub_producto tsp ON tsp.id = tv.id_tipo_sub_producto
-        LEFT JOIN tipo_producto tp ON tp.id = tsp.id_tipo_producto
-        LEFT JOIN _view_remito_precio_total precio_total ON precio_total.id_remito=r.id
-        LEFT JOIN estado_remito er ON er.id = r.id_estado_remito
+        er.id                                                          AS idEstado,
+        ep.nombre                                                      AS estadoPedidoProducto,
+        ep.color                                                       AS colorEstadoPedidoProducto,
+        ep.id                                                          AS idEstadoPedidoProducto
+    FROM reserva r
+             LEFT JOIN pedido_producto pp ON pp.id = r.id_pedido_producto
+             LEFT JOIN pedido p ON p.id = pp.id_pedido
+             LEFT JOIN usuario ur ON r.id_cliente = ur.id
+             LEFT JOIN usuario u ON p.id_cliente = u.id
+             LEFT JOIN tipo_variedad tv ON tv.id = pp.id_tipo_variedad
+             LEFT JOIN tipo_sub_producto tsp ON tsp.id = tv.id_tipo_sub_producto
+             LEFT JOIN tipo_producto tp ON tp.id = tsp.id_tipo_producto
+             LEFT JOIN estado_reserva er ON er.id = r.id_estado
+             LEFT JOIN estado_pedido_producto ep ON ep.id = pp.id_estado_pedido_producto
     WHERE r.fecha_baja IS NULL
       AND (r.fecha_creacion >= _fechaDesde AND r.fecha_creacion <= _fechaHasta)
       AND (_idCliente IS NULL OR (_idCliente IS NOT NULL AND r.id_cliente = _idCliente))
     ORDER BY r.id DESC
+    ;
+END;");
+        $statement->execute();
+
+        $statement = $connection->prepare("create
+    definer = root@localhost procedure sp_index_pedido(IN _fechaDesde datetime, IN _fechaHasta datetime, IN _idCliente int)
+BEGIN
+    SELECT DISTINCT
+        p.id                                                        AS id,
+        pp.id                                                       AS idProducto,
+        p.fecha_creacion                                            AS fechaCreacion,
+        tv.nombre                                                   AS nombreVariedad,
+        tp.nombre                                                   AS nombreProducto,
+        tsp.nombre                                                  AS nombreSubProducto,
+        CONCAT(tp.nombre,' ',tsp.nombre,' ',tv.nombre)              AS nombreProductoCompleto,
+        CONCAT(u.nombre,', ',u.apellido)                            AS cliente,
+        u.id                                                        AS idCliente,
+        CONCAT(pp.cantidad_bandejas_pedidas,' (x',tb.nombre,')')    AS cantidadBandejas,
+        tb.nombre                                                   AS tipoBandeja,
+        pp.fecha_siembra_pedido                                     AS fechaSiembraPedido,
+        pp.fecha_entrega_pedido                                     AS fechaEntregaPedido,
+        epp.nombre                                                  AS estado,
+        epp.color                                                   AS colorEstado,
+        epp.id                                                      AS idEstado,
+        if(pp.fecha_salida_camara_real is null, (to_days(curdate()) - to_days(cast(`pp`.`fecha_entrada_camara` as date))),
+           (to_days(pp.fecha_salida_camara_real) - to_days(pp.fecha_entrada_camara))) AS `diasEnCamara`,
+        if(epp.id in (5,6), (
+            if(pp.fecha_entrega_pedido_real is null, (to_days(curdate()) - to_days(cast(`pp`.`fecha_salida_camara_real` as date))),
+               (to_days(pp.fecha_entrega_pedido_real) - to_days(pp.fecha_salida_camara_real)))
+            ), '-') AS `diasEnInvernaculo`,
+        CONCAT(pp.numero_orden,' ',substr(`tp`.`nombre`, 1, 3))  AS ordenSiembra,
+        if(tm2.numero is null, tm1.numero, CONCAT(tm1.numero,' / ',tm2.numero)) AS mesada
+    FROM pedido p
+             LEFT JOIN pedido_producto pp ON pp.id_pedido = p.id
+             LEFT JOIN tipo_variedad tv on tv.id = pp.id_tipo_variedad
+             LEFT JOIN tipo_sub_producto tsp on tsp.id = tv.id_tipo_sub_producto
+             LEFT JOIN tipo_producto tp on tp.id = tsp.id_tipo_producto
+             LEFT JOIN tipo_bandeja tb on (tb.id = pp.id_tipo_bandeja)
+             LEFT JOIN usuario u ON (u.id = p.id_cliente)
+             LEFT JOIN estado_pedido_producto epp ON (epp.id = pp.id_estado_pedido_producto)
+             LEFT JOIN mesada m1 on (m1.id = pp.id_mesada_uno)
+             LEFT JOIN tipo_mesada tm1 on (tm1.id = m1.id_tipo_mesada)
+             LEFT JOIN mesada m2 on (m2.id = pp.id_mesada_dos)
+             LEFT JOIN tipo_mesada tm2 on (tm2.id = m2.id_tipo_mesada)
+    WHERE p.fecha_baja IS NULL
+      AND (p.fecha_creacion >= _fechaDesde AND p.fecha_creacion <= _fechaHasta)
+      AND (_idCliente IS NULL OR (_idCliente IS NOT NULL AND p.id_cliente = _idCliente))
+        ORDER BY p.id DESC
     ;
 END;");
         $statement->execute();
@@ -911,8 +962,7 @@ END;");
             INSERT INTO babyplant2.tipo_sub_producto (id, id_usuario_creacion, id_usuario_ultima_modificacion, id_usuario_deshabilito, nombre, descripcion, fecha_creacion, fecha_ultima_modificacion, fecha_baja, codigo_interno, habilitado, fecha_deshabilitado, id_tipo_producto) VALUES (154, 1, null, null, 'EG-5204 F1', null, '2024-08-31 14:16:28', '2024-08-31 14:16:28', null, null, 1, null, 6);
             INSERT INTO babyplant2.tipo_sub_producto (id, id_usuario_creacion, id_usuario_ultima_modificacion, id_usuario_deshabilito, nombre, descripcion, fecha_creacion, fecha_ultima_modificacion, fecha_baja, codigo_interno, habilitado, fecha_deshabilitado, id_tipo_producto) VALUES (155, 1, null, null, 'PRINCE', null, '2024-08-31 14:16:28', '2024-08-31 14:16:28', null, null, 1, null, 1);
             INSERT INTO babyplant2.tipo_sub_producto (id, id_usuario_creacion, id_usuario_ultima_modificacion, id_usuario_deshabilito, nombre, descripcion, fecha_creacion, fecha_ultima_modificacion, fecha_baja, codigo_interno, habilitado, fecha_deshabilitado, id_tipo_producto) VALUES (156, 1, null, null, 'COLORADA DE INVIERNO', null, '2024-08-31 14:16:28', '2024-08-31 14:16:28', null, null, 1, null, 17);
-            INSERT INTO babyplant2.tipo_sub_producto (id, id_usuario_creacion, id_usuario_ultima_modificacion, id_usuario_deshabilito, nombre, descripcion, fecha_creacion, fecha_ultima_modificacion, fecha_baja, codigo_interno, habilitado, fecha_deshabilitado, id_tipo_producto) VALUES (157, 1, 1, null, 'ACELGA 2', null, '2024-11-25 13:12:24', '2024-11-25 13:12:24', null, null, 1, null, 7);
-            ");
+");
         $statement->execute();
 
 
