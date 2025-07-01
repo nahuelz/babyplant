ab4f950abb0d056a7941d0d83cb66d822c34e4ba
nahuelz <nahuel.zanelli@hotmail.com>
2025-07-01 21:30:21 +0000

    Caja

--- src/SQLViews/routines/sp_index_movimiento.sql
+++ src/SQLViews/routines/sp_index_movimiento.sql
@@ -4,8 +4,13 @@ create
 BEGIN
     SELECT DISTINCT
         m.id AS id,
-        ifnull(uccp.id, uccu.id) AS idCliente
+        m.monto AS monto,
+        tm.nombre AS tipoMovimiento,
+        ifnull(uccp.id, uccu.id) AS idCliente,
+        CONCAT((ifnull(uccp.nombre, uccu.nombre)), ' ', (ifnull(uccp.apellido, uccu.apellido))) AS nombreCliente,
+        m.fecha_creacion AS fechaCreacion
     FROM movimiento m
+             LEFT JOIN tipo_movimiento tm ON tm.id = m.id_tipo_movimiento
              LEFT JOIN cuenta_corriente_pedido ccp ON m.id_cuenta_corriente_pedido = ccp.id
              LEFT JOIN pedido p ON ccp.id = p.id_cuenta_corriente_pedido
              LEFT JOIN usuario uccp ON uccp.id = p.id_cliente
@@ -14,6 +19,7 @@ BEGIN
     WHERE m.fecha_baja IS NULL
       AND (m.fecha_creacion >= _fechaDesde AND m.fecha_creacion <= _fechaHasta)
       AND (_idCliente IS NULL OR (_idCliente IS NOT NULL AND (uccp.id = _idCliente OR uccu.id = _idCliente)))
+      AND (tm.id = 1 or tm.id = 3)
     ORDER BY m.id DESC
     ;
 END;
