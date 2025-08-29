9406a8564d0c72c70116e1f3826421b234182144
nahuelz <nahuel.zanelli@hotmail.com>
2025-08-29 17:19:09 +0000

    Fix remito entrega

--- src/Entity/PedidoProducto.php
+++ src/Entity/PedidoProducto.php
@@ -62,7 +62,7 @@ class PedidoProducto {
     private mixed $cantidadBandejasReales;
 
     /**
-     * @ORM\Column(name="cantidad_bandejas_disponibles", type="integer", nullable=true)
+     * @ORM\Column(name="cantidad_bandejas_disponibles", type="decimal", precision=5, scale=1, nullable=true)
      */
     private mixed $cantidadBandejasDisponibles;
 
