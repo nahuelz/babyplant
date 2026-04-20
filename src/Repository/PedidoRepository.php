2d6b59fc29b1cf3fe4059dbf4a5dc8600d71666d
nahuelz <nahuel.zanelli@hotmail.com>
2026-04-20 18:05:00 +0000

    ranking clientes

--- src/Repository/PedidoRepository.php
+++ src/Repository/PedidoRepository.php
@@ -22,12 +22,22 @@ class PedidoRepository extends ServiceEntityRepository {
     public function getPedidosPorCliente(\DateTime $desde, \DateTime $hasta): array
     {
         return $this->createQueryBuilder('p')
-            ->select('u.id, u.nombre, u.apellido, COUNT(p.id) AS cantidad')
+            ->select('
+                u.id, 
+                u.nombre, 
+                u.apellido, 
+                COUNT(p.id) AS cantidad,
+                COALESCE(SUM(pp.cantidadBandejasPedidas), 0) AS total_bandejas
+            ')
             ->join('p.cliente', 'u')
+            ->leftJoin('p.pedidosProductos', 'pp')
             ->where('p.fechaCreacion BETWEEN :desde AND :hasta')
+            ->andWhere('UPPER(u.nombre) NOT LIKE :stockFilter')
+            ->andWhere('UPPER(u.apellido) NOT LIKE :stockFilter')
             ->setParameter('desde', $desde->format('Y-m-d 00:00:00'))
             ->setParameter('hasta', $hasta->format('Y-m-d 23:59:59'))
-            ->groupBy('u.id')
+            ->setParameter('stockFilter', '%STOCK%')
+            ->groupBy('u.id, u.nombre, u.apellido')
             ->orderBy('cantidad', 'DESC')
             ->getQuery()
             ->getArrayResult();
