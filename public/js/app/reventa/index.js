4a11f87c6915a301cb940ad88681b4168c45f931
nahuelz <nahuel.zanelli@hotmail.com>
2026-09-15 16:57:01 +0000

    fix orden reventas

--- public/js/app/reventa/index.js
+++ public/js/app/reventa/index.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
         fixedHeader: false,
         destroy: true,
         columnDefs: [
-            {targets: 0, width: '1%'},
+            {targets: 0, width: '1%', type: 'num'},
             {targets: 4, width: '10%'},
             {targets: 10, width: '10%'},
             {targets: 11, width: '2%', orderable: false}
