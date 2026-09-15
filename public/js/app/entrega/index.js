7c8012be8efefb164f72b8772214865b60f5ba24
nahuelz <nahuel.zanelli@hotmail.com>
2026-09-15 19:17:04 +0000

    cancelar entrega

--- public/js/app/entrega/index.js
+++ public/js/app/entrega/index.js
@@ -281,7 +281,7 @@ function dataTablesActionFormatter(data, type, full, meta) {
             +
             (data.print_pdf_interno !== undefined ? '<a class="dropdown-item" href="' + data.print_pdf_interno + '"><i class="la la-file-pdf" style="margin-right: 5px;"></i> Imprimir Entrega Interno</a>' : '')
             +
-            (data.delete !== undcanceefined ? '<a class="dropdown-item accion-borrar" href="' + data.delete + '"><i class="la la-remove" style="margin-right: 5px;"></i> Borrar</a>' : '')
+            (data.delete !== undefined ? '<a class="dropdown-item accion-borrar" href="' + data.delete + '"><i class="la la-remove" style="margin-right: 5px;"></i> Borrar</a>' : '')
             +
             (data.cancelar !== undefined ? '<a class="dropdown-item accion-cancelar" href="' + data.cancelar + '" data-es-reventa="' + (data.es_reventa || false) + '"><i class="la la-remove" style="margin-right: 5px;"></i> Cancelar </a>' : '')
         ;
